51287c1a63524f8e2bb2ed281ac7e68bf08db1dd
prabpypipo <46016382+prabpypipo@users.noreply.github.com>
2021-11-14 00:21:36 +0800

    Initial Commit

---
+++ Hello Color/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hello Color
+//
+//  Created by Nawapon Sangsiri on 14/11/2564 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hello Color/Hello_ColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hello_ColorApp.swift
+//  Hello Color
+//
+//  Created by Nawapon Sangsiri on 14/11/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct Hello_ColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
